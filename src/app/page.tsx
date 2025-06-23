429764292cc800e5b2ce89b6c59fbd363d5277a5
Shaurya Maan Gusain <smgusain24@gmail.com>
2025-06-24 02:37:51 +0530

    Fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState, useMemo } from 'react'
 import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
 
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import NewsLetterSubscribe from './NewsLetterSubscribe'
 
 // Animated background spheres
